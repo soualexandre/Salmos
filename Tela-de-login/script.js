725f00920a36d8150aae8df30616826e48237da8
Alexandre Souza dos Santos <55295879+xande-dev@users.noreply.github.com>
2019-09-13 17:35:03 -0300

    Delete script.js

--- Tela-de-login/script.js
+++
@@ -1,7 +0,0 @@
-const btnLogin = document.querySelector(".btn-login");
-const form = document.querySelector("form");
-
-btnLogin.addEventListener("click", evnt => {
-	event.preventDefault();
-	form.classList.add("form-hide");
-})
